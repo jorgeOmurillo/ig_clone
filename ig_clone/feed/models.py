1d047343c05e21dca88af7ef045a6d467d1ec44a
Jorge Murillo <j.orlandomurillo@gmail.com>
2017-08-25 16:54:11 +0800

    user model

--- ig_clone/feed/models.py
+++ ig_clone/feed/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    
+    def __str__(self):
+        return self.user.username
+
+
 class FileIt(models.Model):
-    user_up = models.ForeignKey('auth.User')
     description = models.CharField(max_length=255, blank=True)
     file_it = models.FileField(upload_to='images/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
+
