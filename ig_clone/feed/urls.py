a8009647d62137d1cad164d1212ee96fe602e273
jorgeOmurillo <j.orlandomurillo@gmail.com>
2017-08-28 21:17:13 +0800

    user changes

--- ig_clone/feed/urls.py
+++ ig_clone/feed/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     # url(r'^uploads/form/$', views.model_form_upload, name='model_form_upload'),
     url(r'^login/$', views.login_user, name='login'),
-    url(r'^signup/$', views.logout, name='logout'),
+    url(r'^signup/$', views.signup, name='signup'),
     url(r'^signout/$', views.signout, name='signout'),
+    url(r'^signup_success/$', views.signup_success, name='signup_success'),
 ]
