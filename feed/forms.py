563b030e52fe9b35cced669d4e02ab891748ed9d
jorgeOmurillo <j.orlandomurillo@gmail.com>
2018-01-28 19:50:28 +0800

    heroku ready

---
+++ feed/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+from django.forms import ModelForm
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+from imagekit.forms import ProcessedImageField
+
+from .models import UserID, PostIt
+
+class UserNewForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super(UserNewForm, self).save(commit=False)
+        user.email = self.cleaned_data["email"]
+
+        if commit:
+            user.save()
+        return user
+
+class UserProfilePic(ModelForm):
+    class Meta:
+        model = UserID
+        fields = ['pic_profile', 'description']
+
+class PostPicture(ModelForm):
+    class Meta:
+        model = PostIt
+        fields = ['description', 'image']
---
+++ feed/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.home, name='home'),
+    # url(r'^uploads/form/$', views.model_form_upload, name='model_form_upload'),
+    url(r'^login/$', views.login_user, name='login'),
+    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^signout/$', views.signout, name='signout'),
+    url(r'^signup_success/$', views.signup_success, name='signup_success'),
+    url(r'^profile/(?P<username>[-_\w.]+)/$', views.profile, name='profile'),
+    url(r'^profile/(?P<username>[-_\w.]+)/edit/$', views.profile_settings, name='profile_settings'),
+    url(r'^post/$', views.post_picture, name='post_picture'),
+]
